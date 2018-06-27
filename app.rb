15de1e6db8553a78b846e2d08b5c67890e8d5839
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-27 20:44:51 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,6 +4,16 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
 
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+
+      @ships = Ship.all
+      
+      erb :show
+    end
   end
 end
